ba320e90e0d6b6da54ae4930daf7267df8bff241
quanghoaluu1 <hoalqse183138@fpt.edu.vn>
2025-05-21 09:13:55 +0700

    add customer entity

---
+++ MetroTicketBE.Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using MetroTicket.Domain.Entities;
+
+namespace MetroTicketBE.Domain.Entities;
+
+public class Customer
+{
+    public Guid Id { get; set; }
+    public string UserId { get; set; }
+    public Guid CustomerTypeId { get; set; }
+    public Guid MembershipId { get; set; }
+    public long Points { get; set; }
+    
+    public User User { get; set; } = null!;
+    public CustomerType CustomerType { get; set; } = null!;
+    public Membership Membership { get; set; } = null!;
+}
