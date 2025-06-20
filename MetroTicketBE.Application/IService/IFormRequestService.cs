58c9c3e329d465f827b02492576f92624af4bbdb
hoangtuzami <hoangtuzami@gmail.com>
2025-06-20 16:31:56 +0700

    Create functions which get by user and get all

--- MetroTicketBE.Application/IService/IFormRequestService.cs
+++ MetroTicketBE.Application/IService/IFormRequestService.cs
@@ -1,5 +1,6 @@
 ﻿using MetroTicketBE.Domain.DTO.Auth;
 using MetroTicketBE.Domain.DTO.FormRequest;
+using MetroTicketBE.Domain.Enum;
 using System.Security.Claims;
 
 namespace MetroTicketBE.Application.IService
@@ -7,5 +8,7 @@ namespace MetroTicketBE.Application.IService
     public interface IFormRequestService
     {
         Task<ResponseDTO> SendFormRequest(ClaimsPrincipal user, CreateFormRequestDTO createFormRequestDTO);
+        Task<ResponseDTO> GetFormRequest(ClaimsPrincipal user);
+        Task<ResponseDTO> GetAll(string sortBy, FormStatus formStatus, bool? isAcsending, int pageNumber, int pageSize);
     }
 }
