d6a39c90562a9ab14c2fbfbd4ab8c82a0b58a8ba
hoangtuzami <hoangtuzami@gmail.com>
2025-06-22 19:12:45 +0700

    test signalR

--- MetroTicketBE.Infrastructure/SignalR/CustomUserIdProvider.cs
+++ MetroTicketBE.Infrastructure/SignalR/CustomUserIdProvider.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Security.Claims;
+using Microsoft.Extensions.Logging;
 
 namespace MetroTicketBE.Infrastructure.SignalR
 {
     public class CustomUserIdProvider : IUserIdProvider
     {
+        private readonly ILogger<CustomUserIdProvider> _logger;
+
+        public CustomUserIdProvider(ILogger<CustomUserIdProvider> logger)
+        {
+            _logger = logger;
+        }
+
         public string? GetUserId(HubConnectionContext connection)
         {
             var userId = connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            Console.WriteLine($"[SignalR] Connected UserId: {userId}");
+            _logger.LogInformation("[SignalR] Connected UserId: {UserId}", userId);
             return userId;
         }
     }
