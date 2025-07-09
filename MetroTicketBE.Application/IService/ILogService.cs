e70ab1ea0423b72c7c1479cb28c5691c2b0438c8
quanghoaluu1 <quanghoaluu1@gmail.com>
2025-07-09 22:11:01 +0700

    Enhance log retrieval methods with pagination support

--- MetroTicketBE.Application/IService/ILogService.cs
+++ MetroTicketBE.Application/IService/ILogService.cs
@@ -7,7 +7,7 @@ public interface ILogService
 {
     Task AddLogAsync(LogType logType, string userId, string entityName, string? additionalInfo);
     Task<ResponseDTO> GetAllLogs(int pageNumber, int pageSize);
-    Task<ResponseDTO> GetLogsByCreatedAtRange(DateTime startDate, DateTime endDate);
-    Task<ResponseDTO> GetLogsByLogType(LogType logType);
-    Task<ResponseDTO> GetLogsByUserId(string userId);
+    Task<ResponseDTO> GetLogsByCreatedAtRange(DateTime startDate, DateTime endDate, int pageNumber, int pageSize);
+    Task<ResponseDTO> GetLogsByLogType(LogType logType, int pageNumber, int pageSize);
+    Task<ResponseDTO> GetLogsByUserId(string userId, int pageNumber, int pageSize);
 }
