e18dc0702d46f42c5da3186bf22d85e3ec4b19d8
quanghoaluu1 <quanghoaluu1@gmail.com>
2025-07-09 17:04:14 +0700

    Add IsActive property to GetStationDTO for active status tracking

--- MetroTicketBE.Domain/DTO/Station/GetStationDTO.cs
+++ MetroTicketBE.Domain/DTO/Station/GetStationDTO.cs
@@ -6,6 +6,7 @@ public class GetStationDTO
     public string Name { get; set; } = null!;
     public string? Address { get; set; }
     public string? Description { get; set; }
+    public bool IsActive { get; set; }
     public List<MetroLineDTO> MetroLines { get; set; } = new List<MetroLineDTO>();
 }
 
