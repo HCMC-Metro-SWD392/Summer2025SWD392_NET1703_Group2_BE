1f1ff0950f12f0b1bca6b5f0322a046588cf8765
quanghoaluu1 <quanghoaluu1@gmail.com>
2025-07-09 21:59:30 +0700

    Add Log Directory

---
+++ MetroTicketBE.Domain/DTO/Log/GetLogDTO.cs
@@ -0,0 +1,11 @@
+﻿using MetroTicketBE.Domain.Enum;
+
+namespace MetroTicketBE.Domain.DTO.Log;
+
+public class GetLogDTO
+{
+    public string Description { get; set; } = string.Empty;
+    public string UserFullname { get; set; } = string.Empty;
+    public string LogType { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
