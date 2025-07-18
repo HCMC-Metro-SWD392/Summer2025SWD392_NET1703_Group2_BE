ee95671d902611f6f1082234d0947a7134a9637e
hoangtuzami <hoangtuzami@gmail.com>
2025-07-18 20:08:38 +0700

    Add signalR to login that force logout when other login

---
+++ MetroTicketBE.Application/SignalR/NotificationHub.cs
@@ -0,0 +1,31 @@
+﻿using MetroTicketBE.Application.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MetroTicketBE.Domain.Entities
+{
+    [Authorize]
+    public class NotificationHub : Hub
+    {
+        private readonly IRedisService _redis;
+
+        public NotificationHub(IRedisService redis)
+        {
+            _redis = redis;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;
+            await _redis.StoreKeyAsync($"checkLogin:{userId}", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var userId = Context.UserIdentifier;
+            await _redis.DeleteKeyAsync($"checkLogin:{userId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
